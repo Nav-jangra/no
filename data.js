df0dc84180cf2b007cd007ac5d982c28b5b0cd41
Naveen jangra <90304808+Nav-jangra@users.noreply.github.com>
2022-03-26 17:24:48 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,33 +2,57 @@ links= {
     Monday : ['https://teams.microsoft.com/l/meetup-join/19%3aDZlRn8Tj4tBrFkGvhpyqGyAOuKu2DZrLHoUnsCfdfpU1%40thread.tacv2/1644074914875?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22c7997a82-aa95-4298-b064-0789ab99792a%22%7d',
  'https://teams.microsoft.com/l/meetup-join/19%3aXKGrPQYGR43hN_sIvz-Rz46_q_YvnhxzXGCtTFTJmdU1%40thread.tacv2/1644212081894?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22f23dbf9b-2d60-42af-8828-3054a430e3d4%22%7d', 
 'https://teams.microsoft.com/l/meetup-join/19%3aML_FTT7eFRz156WvHGA5AWTXArjVJO6TPiOglVJBS2o1%40thread.tacv2/1644210952988?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%229ad97ddd-4e55-4473-9411-cf03162846e4%22%7d', 
-'https://teams.microsoft.com/l/meetup-join/19%3aHzVMg96HnYtLxmKmHpYbuMmrN50vl6gIeVmys1hiUoM1%40thread.tacv2/1645964528021?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22724e29fe-01e2-46dc-8333-ccaa8d45e033%22%7d', '', '', 
+'https://teams.microsoft.com/l/meetup-join/19%3aHzVMg96HnYtLxmKmHpYbuMmrN50vl6gIeVmys1hiUoM1%40thread.tacv2/1645964528021?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22724e29fe-01e2-46dc-8333-ccaa8d45e033%22%7d', 
+              '', 
+              '', 
 'https://teams.microsoft.com/l/meetup-join/19%3aSiotag14QKB8ZYwhG_0lDKUp1PkoBU3GcZ2k6cP3DDU1%40thread.tacv2/1644051453805?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%227ff92c37-ddfe-4e06-be78-552e1f42ce70%22%7d',
- 'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'],
+ 'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'
+             ],
+    
+    
     Tuesday: ['https://teams.microsoft.com/l/meetup-join/19%3aCalOJ9Z2hqRiGFPXtnhJA2OD4MWogREbhw4YtButU841%40thread.tacv2/1644044984032?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%227ff92c37-ddfe-4e06-be78-552e1f42ce70%22%7d', 
 'https://teams.microsoft.com/l/meetup-join/19%3aCalOJ9Z2hqRiGFPXtnhJA2OD4MWogREbhw4YtButU841%40thread.tacv2/1644045087392?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%227ff92c37-ddfe-4e06-be78-552e1f42ce70%22%7d', 
 'https://teams.microsoft.com/l/meetup-join/19%3a2t2apQVgBlC4fSYt4ZCennysiAiVzD-kzir1jvlcOaU1%40thread.tacv2/1644075211918?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22c7997a82-aa95-4298-b064-0789ab99792a%22%7d', 
-'https://teams.microsoft.com/l/meetup-join/19%3a2t2apQVgBlC4fSYt4ZCennysiAiVzD-kzir1jvlcOaU1%40thread.tacv2/1644075263744?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22c7997a82-aa95-4298-b064-0789ab99792a%22%7d', '', 
-'https://teams.microsoft.com/l/meetup-join/19%3aFwpDiG6pbQfIfuXWQlSU97Ic6hHqoxTXmJX3F02sSco1%40thread.tacv2/1644042834671?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%227ff92c37-ddfe-4e06-be78-552e1f42ce70%22%7d', '',
-'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'],
+'https://teams.microsoft.com/l/meetup-join/19%3a2t2apQVgBlC4fSYt4ZCennysiAiVzD-kzir1jvlcOaU1%40thread.tacv2/1644075263744?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22c7997a82-aa95-4298-b064-0789ab99792a%22%7d',
+              '', 
+'https://teams.microsoft.com/l/meetup-join/19%3aFwpDiG6pbQfIfuXWQlSU97Ic6hHqoxTXmJX3F02sSco1%40thread.tacv2/1644042834671?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%227ff92c37-ddfe-4e06-be78-552e1f42ce70%22%7d',
+              '',
+'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'
+             ],
+    
+    
     Wednesday: ['https://teams.microsoft.com/l/meetup-join/19%3a0G7NTHY33gAbu60MGNWW_V4urQCPCvOzUc1AHUlaR1w1%40thread.tacv2/1644210327986?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22f23dbf9b-2d60-42af-8828-3054a430e3d4%22%7d', 
 'https://teams.microsoft.com/l/meetup-join/19%3a0G7NTHY33gAbu60MGNWW_V4urQCPCvOzUc1AHUlaR1w1%40thread.tacv2/1644210466958?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22f23dbf9b-2d60-42af-8828-3054a430e3d4%22%7d', 
-'https://teams.microsoft.com/l/meetup-join/19%3aML_FTT7eFRz156WvHGA5AWTXArjVJO6TPiOglVJBS2o1%40thread.tacv2/1644211051976?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%229ad97ddd-4e55-4473-9411-cf03162846e4%22%7d', '', 
+'https://teams.microsoft.com/l/meetup-join/19%3aML_FTT7eFRz156WvHGA5AWTXArjVJO6TPiOglVJBS2o1%40thread.tacv2/1644211051976?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%229ad97ddd-4e55-4473-9411-cf03162846e4%22%7d',
+                '', 
 'https://teams.microsoft.com/l/meetup-join/19%3aDZlRn8Tj4tBrFkGvhpyqGyAOuKu2DZrLHoUnsCfdfpU1%40thread.tacv2/1644075017980?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22c7997a82-aa95-4298-b064-0789ab99792a%22%7d', 
 'https://teams.microsoft.com/l/meetup-join/19%3aRSzmTM5cv7XCg3_0evf9aJKYRtYfesbziIB5IZsxTYY1%40thread.tacv2/1644214167365?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%2250498d5d-4bcb-44e7-8891-c1ec25db6229%22%7d', 
 'https://teams.microsoft.com/l/meetup-join/19%3aRSzmTM5cv7XCg3_0evf9aJKYRtYfesbziIB5IZsxTYY1%40thread.tacv2/1644214167365?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%2250498d5d-4bcb-44e7-8891-c1ec25db6229%22%7d',
-'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'],
+'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'
+               ],
+    
+    
     Thursday: ['https://teams.microsoft.com/l/meetup-join/19%3aFwpDiG6pbQfIfuXWQlSU97Ic6hHqoxTXmJX3F02sSco1%40thread.tacv2/1644043052064?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%227ff92c37-ddfe-4e06-be78-552e1f42ce70%22%7d', 
 'https://teams.microsoft.com/l/meetup-join/19%3a2t2apQVgBlC4fSYt4ZCennysiAiVzD-kzir1jvlcOaU1%40thread.tacv2/1644132037557?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22c7997a82-aa95-4298-b064-0789ab99792a%22%7d', 
-'https://teams.microsoft.com/l/meetup-join/19%3a2t2apQVgBlC4fSYt4ZCennysiAiVzD-kzir1jvlcOaU1%40thread.tacv2/1644132163960?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22c7997a82-aa95-4298-b064-0789ab99792a%22%7d', '', 
+'https://teams.microsoft.com/l/meetup-join/19%3a2t2apQVgBlC4fSYt4ZCennysiAiVzD-kzir1jvlcOaU1%40thread.tacv2/1644132163960?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22c7997a82-aa95-4298-b064-0789ab99792a%22%7d', 
+               '', 
 'https://teams.microsoft.com/l/meetup-join/19%3aHzVMg96HnYtLxmKmHpYbuMmrN50vl6gIeVmys1hiUoM1%40thread.tacv2/1644164925516?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22724e29fe-01e2-46dc-8333-ccaa8d45e033%22%7d', 
-'', '','https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'],
+               '', 
+               '',
+ 'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'
+              ],
+    
+    
     Friday: ['https://teams.microsoft.com/l/meetup-join/19%3aHzVMg96HnYtLxmKmHpYbuMmrN50vl6gIeVmys1hiUoM1%40thread.tacv2/1645964780901?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22724e29fe-01e2-46dc-8333-ccaa8d45e033%22%7d', 
 'https://teams.microsoft.com/l/meetup-join/19%3aML_FTT7eFRz156WvHGA5AWTXArjVJO6TPiOglVJBS2o1%40thread.tacv2/1644211150956?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%229ad97ddd-4e55-4473-9411-cf03162846e4%22%7d',
- 'https://teams.microsoft.com/l/meetup-join/19%3aFwpDiG6pbQfIfuXWQlSU97Ic6hHqoxTXmJX3F02sSco1%40thread.tacv2/1644044341289?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%227ff92c37-ddfe-4e06-be78-552e1f42ce70%22%7d', '', 
+ 'https://teams.microsoft.com/l/meetup-join/19%3aFwpDiG6pbQfIfuXWQlSU97Ic6hHqoxTXmJX3F02sSco1%40thread.tacv2/1644044341289?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%227ff92c37-ddfe-4e06-be78-552e1f42ce70%22%7d',
+             '', 
 'https://teams.microsoft.com/l/meetup-join/19%3aRSzmTM5cv7XCg3_0evf9aJKYRtYfesbziIB5IZsxTYY1%40thread.tacv2/1644038440194?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%2288d85944-5078-43de-9837-8b28e279b28f%22%7d',
- 'https://teams.microsoft.com/l/meetup-join/19%3aRSzmTM5cv7XCg3_0evf9aJKYRtYfesbziIB5IZsxTYY1%40thread.tacv2/1644038440194?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%2288d85944-5078-43de-9837-8b28e279b28f%22%7d', '',
-'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'],
+ 'https://teams.microsoft.com/l/meetup-join/19%3aRSzmTM5cv7XCg3_0evf9aJKYRtYfesbziIB5IZsxTYY1%40thread.tacv2/1644038440194?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%2288d85944-5078-43de-9837-8b28e279b28f%22%7d',
+             '',
+'https://teams.microsoft.com/l/meetup-join/19%3aMEIUZCFtoTOAG2mK_Q3J8DEDbYUyq85VaEC2P-RtdCE1%40thread.tacv2/1647938582771?context=%7b%22Tid%22%3a%2234bd8bed-2ac1-41ae-9f08-4e0a3f11706c%22%2c%22Oid%22%3a%22744c056f-e4ac-448f-aeef-7fb54547803c%22%7d'
+            ],
+    
     Saturday: [],
     Sunday: []
 
@@ -38,11 +62,11 @@ links= {
 
 
 subjects = {
-    'Monday': ['Java Theory', 'Is', 'Network OS', 'Mobile App dev.', '', '', 'Project','DATA Structure'],
-    Tuesday: ['Technical Training', 'Technical Training', 'Java Lab', 'Java Lab', '', 'Theory of computation', '','DATA Structure'],
-    Wednesday: ['Is Lab', 'Is Lab', 'Network OS', '', 'Java Theory', 'SS', 'SS','DATA Structure'],
-    Thursday: ['Theory of Computation', 'Java Lab', 'Java Lab', '', 'Mobile App dev.', '', '','DATA Structure'],
-    Friday: ['Mobile App dev.', 'Network OS', 'Theory of Computation', '', 'Aptitude', 'Aptitude','','DATA Structure'],
+    'Monday': ['Java Theory', 'Is', 'Network OS', 'Mobile App dev.', '', '', 'Project', 'DATA Structure'],
+    Tuesday: ['Technical Training', 'Technical Training', 'Java Lab', 'Java Lab', '', 'Theory of computation', '', 'DATA Structure'],
+    Wednesday: ['Is Lab', 'Is Lab', 'Network OS', '', 'Java Theory', 'SS', 'SS', 'DATA Structure'],
+    Thursday: ['Theory of Computation', 'Java Lab', 'Java Lab', '', 'Mobile App dev.', '', '', 'DATA Structure'],
+    Friday: ['Mobile App dev.', 'Network OS', 'Theory of Computation', '', 'Aptitude', 'Aptitude','', 'DATA Structure'],
     Saturday: [],
     Sunday: []
 };
